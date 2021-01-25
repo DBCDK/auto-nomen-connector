37dfff4ba8856f04f61fdedda28a78fd2faf5ff2
Anders Thrane Michelsen <atm@dbc.dk>
2021-01-25 13:00:57 +0100

    Add tests
    Auditors: jbn, jbr

---
+++ src/test/java/dk/dbc/autonomen/AutoNomenConnectorTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.autonomen;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import dk.dbc.httpclient.HttpClient;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class AutoNomenConnectorTest {
+    private static WireMockServer wireMockServer;
+    private static String wireMockHost;
+
+    final static Client CLIENT = HttpClient.newClient(new ClientConfig()
+            .register(new JacksonFeature()));
+    static AutoNomenConnector connector;
+
+    @BeforeAll
+    static void startWireMockServer() {
+        wireMockServer = new WireMockServer(options().dynamicPort()
+                .dynamicHttpsPort());
+        wireMockServer.start();
+        wireMockHost = "http://localhost:" + wireMockServer.port();
+        configureFor("localhost", wireMockServer.port());
+    }
+
+    @BeforeAll
+    static void setConnector() {
+        connector = new AutoNomenConnector(CLIENT, wireMockHost);
+    }
+
+    @AfterAll
+    static void stopWireMockServer() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void getSuggestionsTestOkSimple() throws AutoNomenConnectorException {
+        final AutoNomenSuggestions actual = connector.getSuggestions("e6ec1122");
+
+        assertThat(actual.getAutNames().size(), is(1));
+        assertThat(actual.getAutNames().get(0).getInputName(), is("Jens Rebensdorff"));
+        assertThat(actual.getAutNames().get(0).getAuthority(), is("19180557"));
+        assertThat(actual.getNerNames().size(), is(0));
+    }
+
+    @Test
+    void getSuggestionsTestError() {
+        Assertions.assertThrows(AutoNomenConnectorNotFoundException.class, () -> connector.getSuggestions(""), "Article could not be found.");
+        Assertions.assertThrows(AutoNomenConnectorNotFoundException.class, () -> connector.getSuggestions("julemanden"), "Article julemanden could not be found.");
+    }
+}
---
+++ src/test/java/dk/dbc/autonomen/AutoNomenConnectorTestWireMockRecorder.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.autonomen;
+
+public class AutoNomenConnectorTestWireMockRecorder {
+     /*
+        Steps to reproduce wiremock recording:
+
+        * Start standalone runner
+            java -jar wiremock-standalone-{WIRE_MOCK_VERSION}.jar --proxy-all="{AUTO_NOMEN_BASE_URL}" --record-mappings --verbose
+
+        * Run the main method of this class
+
+        * Replace content of src/test/resources/{__files|mappings} with that produced by the standalone runner
+     */
+
+    public static void main(String[] args) throws AutoNomenConnectorException {
+        AutoNomenConnectorTest.connector = new AutoNomenConnector(
+                AutoNomenConnectorTest.CLIENT, "http://localhost:8080");
+        final AutoNomenConnectorTest autoNomenConnectorTest = new AutoNomenConnectorTest();
+        allTests(autoNomenConnectorTest);
+    }
+
+    private static void allTests(AutoNomenConnectorTest autoNomenConnectorTest) throws AutoNomenConnectorException {
+        autoNomenConnectorTest.getSuggestionsTestOkSimple();
+        autoNomenConnectorTest.getSuggestionsTestError();
+    }
+}
